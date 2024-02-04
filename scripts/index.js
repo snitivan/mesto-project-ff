b86443630f9479623beb9e273ca1dd35ea57a472
snitivan <snitivan@gmail.com>
2024-02-04 18:19:49 +0800

    add funtion of card add

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsOnPage = document.querySelector('.places__list');
+
+
+initialCards.forEach(function(item) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = item['link'];
+  cardElement.querySelector('.card__title').textContent = item['name'];
+  cardsOnPage.append(cardElement);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function () {
+    deleteCard(deleteButton);
+  }); 
+})
+
+function deleteCard(deleteButton) {
+  const cardItem = deleteButton.closest('.places__item');
+  cardItem.remove();
+}
+
+
+
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
-
 // @todo: Функция создания карточки
-
 // @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
